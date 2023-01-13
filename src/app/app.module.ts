7eee868e5979196ceeef45363dd1303ef5faf1d3
Maria Brock <brockm@ornl.giv>
2023-01-13 16:36:37 -0500

    completed through two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessComponent } from './components/success/success.component';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
-  serverName  = '';
+  serverName  = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -20,11 +20,13 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is' +  this.serverName;
+
   }
 
   onUpdateServerName(event: any) {
-    this.serverName = event.
+    this.serverName = event.target.value;
+    console.log(event)
   }
 
 }
