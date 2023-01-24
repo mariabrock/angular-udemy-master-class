02d351314537b5f11f96762dacc0e3f300d7a263
Maria Brock <brockm@ornl.giv>
2023-01-24 18:02:42 -0500

    started assignment 3. have implemented toggle and a count increment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,11 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
   serverName  = 'Testserver';
+  serverCreated = false;
+  servers = ['Testeserver', 'Testserver 2'];
+  details: boolean = false;
+  clickCount = 0 ;
+
 
   constructor() {
     setTimeout(() => {
@@ -20,13 +25,19 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is' +  this.serverName;
-
   }
 
   onUpdateServerName(event: any) {
     this.serverName = event.target.value;
     console.log(event)
+  }
+
+  toggle(click) {
+    this.details = !this.details;
+    this.clickCount++;
   }
 
 }
